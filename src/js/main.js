8f6515141913c6c5793128e7a847a55a8ae1de13
João Trepichio <trepichio@gmail.com>
2015-04-22 08:52:02 -0300

    Added some new features, such as:  1-a sidebar containing a list of cats, listed by name  2-an area to display only the data of the selected cat  3-The number of clicks in the cat area is unique to each cat  Fixed click counter starting by 1 instead of 0

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,88 @@
 document.addEventListener( "DOMContentLoaded", function() {
-	var Cats = ['Stuart','Galileo'];
-
-	for (var i=0; i < Cats.length; i++){
-		var img = document.createElement('IMG');
-		img.src = 'src/img/cat_'+i+'.jpg';
-		img.setAttribute('class', 'img');
-
-		var $imgDiv = document.createElement('DIV');
-		$imgDiv.className = 'img-container';
-		$imgDiv.appendChild(img);
-
-		var $clickCount = document.createElement('DIV');
-		$clickCount.className = 'click-counter';
-		$clickCount.id = "counter_"+ i;
-		$imgDiv.appendChild($clickCount);
-
-		document.querySelector('.main-container').appendChild($imgDiv);
-		var clicks = 0;
-
-		$imgDiv.addEventListener('click', (function (c,i) {
-			
-			return function(){
-				var text = "No. Clicks: " + c++;
-				document.querySelector('#counter_'+i)
-					.innerHTML = text;
-			}
-		})(clicks,i), false);
+	function Cat (i) {
+		this.name = catNames[i];
+		this.pic = 'src/img/cat_'+ i +'.jpg';
+		this.clickCount = 0;
 	}
+
+	Cat.prototype.addImage = function() {
+
+		var img = document.createElement('IMG');
+		img.src = this.pic;
+		img.className = 'img';
+
+		var div = document.createElement('DIV');
+		div.className = 'img-container';
+		div.setAttribute('id', this.name);
+		div.appendChild(img);
+		$main.appendChild(div);
+	};
+
+	Cat.prototype.addFoot = function() {
+		var div = document.createElement('DIV');
+		div.className = 'click-counter';
+		div.setAttribute('id', 'counter-'+this.name);
+
+		var $imgDiv = document.querySelector('#'+this.name);
+		$imgDiv.appendChild(div);
+
+	};
+
+	Cat.prototype.addClick = function() {
+		var self = this;
+		document.querySelector('#'+self.name)
+			.addEventListener('click', function () {
+				var text = "No. Clicks: " + ++self.clickCount;
+				document.querySelector('#counter-'+self.name)
+					.innerHTML = text;
+			}, false);
+	};
+
+	Cat.prototype.show = function() {
+		hideAllCats();
+		document.querySelector('#'+this.name).style.display = "";
+		document.querySelector('#counter-'+this.name).style.display = "";
+	};
+
+	function hideAllCats () {
+		var allContainers = document.querySelectorAll('.img-container');
+		var allItems = document.querySelectorAll('.listCats li');
+		for (var x = 0; x < allContainers.length; x++){
+			allContainers[x].style.display = "none";
+			allItems[x].style.color = "white";
+		}
+	}
+
+	var catNames = [
+		'Stuart','Galileo','José','Sofia','Pink'
+	];
+	catNames = catNames.sort();
+
+	var $main = document.querySelector('.main-container');
+	var kitten = [];
+
+	for (each in catNames) {
+		var li = document.createElement('li');
+		li.innerHTML = catNames[each];
+		li.id = "item-" + each;
+		var $ul = document.querySelector('.listCats');
+		$ul.appendChild(li);
+
+		kitten[each] = new Cat(each);
+		kitten[each].addImage();
+		kitten[each].addFoot();
+		kitten[each].addClick();
+
+		li.addEventListener('click',(function (copyEach) {
+
+			return function(){
+				kitten[copyEach].show();
+				this.style.color = "green";
+
+			};
+		})(each));
+	}
+
+	hideAllCats();
 
 }, false);
